0b959238281e84f0fe9ccc37d74c8e71264d9843
Karan Saini <karansaini.art@gmail.com>
2026-04-09 23:13:59 +0530

    connecting local files to repo

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("hello world");
+console.error("this is an error");
+console.warn("this is a warning");
+
+const a = 10;
+const b = 15;
+
+console.log(`The sum of a and b is ${a + b}`);
+
+//karan//
